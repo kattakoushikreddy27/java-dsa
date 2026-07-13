90a69bc6e3fd406432719386d8217fac6ffa87f3
Katta Koushik Reddy <kattak087@gmail.com>
2026-07-14 00:17:53 +0530

    Day 2: Add HelloWorld example

---
+++ Day2/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, DSA!");
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+}
